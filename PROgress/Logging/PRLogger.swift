bb84164f2270bac927fe3058c7f5e0187282ac71
Balazs Erdesz <ispain17@yahoo.com>
2023-08-07 20:15:44 +0200

    corrected image fetching code to account for thread pool

---
+++ PROgress/Logging/PRLogger.swift
@@ -0,0 +1,16 @@
+//
+//  PGLogger.swift
+//  PROgress
+//
+//  Created by Balázs Erdész on 2023. 08. 03..
+//
+
+import Foundation
+import os
+
+/// The collection of `Logger` instances the app uses.
+///
+/// - Important: Always use the logger corresponding to the correct category for logging events.
+enum PRLogger {
+    static let app = Logger(subsystem: "com.ebuniapps.PROgress", category: "app")
+}
